bf5b8c0cda59b6446e5321a83c24948bb459e79b
Meet Gandhi <meetgandhi692@gmail.com>
2022-08-31 16:07:00 +0530

    Add

---
+++ lib/productDetails.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatefulWidget {
+  const ProductDetails({Key? key}) : super(key: key);
+
+  @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+      ),
+    );
+  }
+}
