9470f70339f2054384ef401b7398aea17562803c
Meet Gandhi <meetgandhi692@gmail.com>
2022-09-05 11:25:31 +0530

    Added

--- lib/productDetails.dart
+++ lib/productDetails.dart
@@ -20,175 +20,226 @@ class _ProductDetailsState extends State<ProductDetails> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         elevation: 2,
-        backgroundColor: Colors.blue,
-        child: Icon(Icons.shopping_basket_rounded),
+        backgroundColor: Colors.white,
+        child: const Icon(
+          Icons.shopping_basket_rounded,
+          color: Colors.blue,
+        ),
       ),
       body: SafeArea(
-        child: Container(
-          child: Stack(
-            children: [
-              Container(
-                // color: Colors.white,
-                height: size.height * 0.4,
-                alignment: Alignment.center,
-                // color: Colors.white,
-                child: Image.asset('assets/images/sofa_3.jpg'),
-              ),
-              DraggableScrollableSheet(
-                maxChildSize: .68,
-                initialChildSize: .6,
-                minChildSize: .6,
-                builder: (context, scrollController) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                      ),
-                      color: Color.fromRGBO(44, 53, 57, 1),
+        child: Stack(
+          children: [
+            Container(
+              height: size.height * 0.35,
+              alignment: Alignment.center,
+              // color: Colors.white,
+              child: Image.asset('assets/images/sofa_3.jpg'),
+            ),
+            DraggableScrollableSheet(
+              maxChildSize: .72,
+              initialChildSize: .63,
+              minChildSize: .63,
+              builder: (context, scrollController) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
                     ),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          const SizedBox(height: 10),
-                          Container(
-                            alignment: Alignment.center,
-                            child: Container(
-                              width: 50,
-                              height: 5,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade50,
-                                borderRadius: const BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
+                    color: Color.fromRGBO(44, 53, 57, 1),
+                  ),
+                  child: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        buildSizedBox(10),
+                        Container(
+                          alignment: Alignment.center,
+                          child: Container(
+                            width: 50,
+                            height: 5,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade50,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
                               ),
                             ),
                           ),
-                          const SizedBox(height: 18),
-                          Container(
-                            padding: EdgeInsets.only(left: 18),
-                            alignment: Alignment.centerLeft,
-                            child: const Text(
-                              'GAMMALBYN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                          ),
-                          const SizedBox(height: 6),
-                          Container(
-                            padding: const EdgeInsets.only(left: 18.0),
-                            alignment: Alignment.centerLeft,
-                            child: const Text(
-                              '3-seat sofa, grey',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w100),
-                            ),
-                          ),
-                          const SizedBox(height: 14),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 18.0),
-                            child: Text(
-                              'Rs. 25,000',
-                              style: TextStyle(
-                                fontSize: 38,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 28),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 18.0),
-                            child: Text(
-                              'Product Information',
-                              style: TextStyle(
+                        ),
+                        buildSizedBox(18),
+                        Container(
+                          padding: const EdgeInsets.only(left: 18),
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            'GAMMALBYN',
+                            style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        buildSizedBox(6),
+                        Container(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            '3-seat sofa, grey',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w100),
+                          ),
+                        ),
+                        buildSizedBox(14),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 18.0),
+                          child: Text(
+                            'Rs. 25,000',
+                            style: TextStyle(
+                              fontSize: 38,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
-                          const SizedBox(height: 14),
-                          Container(
-                            alignment: Alignment.center,
-                            child: Container(
-                              width: 380,
-                              height: 1,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade50,
-                              ),
+                        ),
+                        buildSizedBox(28),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 18.0),
+                          child: Text(
+                            'Product Information',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 18, vertical: 24),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  'Reviews',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15.5,
+                        ),
+                        buildSizedBox(14),
+                        buildDivider(),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 18, vertical: 24),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                'Reviews',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.5,
+                                ),
+                              ),
+                              Row(
+                                children: const <Widget>[
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 18,
                                   ),
-                                ),
-                                Row(
-                                  children: const <Widget>[
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 18,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 18,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 18,
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
-                                      size: 18,
-                                    ),
-                                    Icon(
-                                      Icons.star_border,
-                                      color: Colors.white,
-                                      size: 18,
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 18,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 18,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                    size: 18,
+                                  ),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Colors.white,
+                                    size: 18,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        buildDivider(),
+                        ExpansionTile(
+                          title: const Text(
+                            'Product details',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.5,
                             ),
                           ),
-                          Container(
-                            alignment: Alignment.center,
-                            child: Container(
-                              width: 380,
-                              height: 1,
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade50,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(
+                                  left: 17, right: 17, bottom: 10),
+                              width: 400,
+                              child: const Text(
+                                'MRP Rs.36,733 (incl. tax)\n\nSeat cushions filled with high resilience foam and polyester fibre wadding give comfortable support for your body, and easily regain their shape when you get up.\n\nBack cushions filled with polyester fibres for soft comfort.\n\nCountry of Origin - India',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w200,
+                                ),
                               ),
                             ),
+                          ],
+                        ),
+                        buildDivider(),
+                        ExpansionTile(
+                          title: const Text(
+                            'Materials',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.5,
+                            ),
                           ),
-                        ],
-                      ),
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(
+                                  left: 17, right: 17, bottom: 10),
+                              width: 400,
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    'Leg',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w200,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        buildDivider(),
+                      ],
                     ),
-                  );
-                },
-              ),
-            ],
-          ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildSizedBox(double h) => SizedBox(height: h);
+
+  Container buildDivider() {
+    return Container(
+      alignment: Alignment.center,
+      child: Container(
+        width: 400,
+        height: 1,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade50,
         ),
       ),
     );
@@ -208,7 +259,7 @@ class _ProductDetailsState extends State<ProductDetails> {
       ),
       actions: [
         Padding(
-          padding: const EdgeInsets.only(top: 8, right: 25.0),
+          padding: const EdgeInsets.only(top: 9, right: 25.0),
           child: LikeButton(
             onTap: onLikeButtonTapped,
           ),
@@ -225,9 +276,9 @@ class _ProductDetailsState extends State<ProductDetails> {
         //   ),
         // ),
         Padding(
-          padding: EdgeInsets.only(top: 10, right: 20.0),
+          padding: const EdgeInsets.only(top: 9, right: 20.0),
           child: Badge(
-            badgeContent: Text('0'),
+            badgeContent: const Text('0'),
             child: SvgPicture.asset("assets/svg/cart.svg"),
           ),
         ),
